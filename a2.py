3dddcf89a916301809293a86b60b6269e025ad6f
DWitol <Devon_Witol@live.com>
2017-10-24 20:25:42 -0400

    starting deff of classes

--- a2.py
+++ a2.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy import stats
 from sklearn.datasets import fetch_mldata
+import random
+
 
 #Basics:
 #load mnist dataset
@@ -14,10 +16,30 @@ print(b)
 print(c)
 # Question 1
 
-#neuron definition
-def node:
+class Node(object):
+    prevLayer = []
+    nextLayer = []
+    output = 0
+
+    def output():
+        for i in prevLayer:
+            output += prevLayer[i].output()
+            
+
+
+class NeuralNetwork(object):
+
+    def __init__(self,n_nodes,n_layers = 3):
+        layers = [n_layers]
+
+
+    def make_student():
+        student = NeuralNetwork()
+        return network
+
 
 print("Question 1: ")
+
 
 # Question 2
 print("Question 2: ")
