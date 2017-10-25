12f086056dcd5d2d289e15a84c40c08494857f78
Kelly Maclauchlan <kelly maclauchlan>
2017-10-25 16:46:27 -0400

    added in functions for the nural network

--- a2.py
+++ a2.py
@@ -13,10 +13,11 @@ mnist = fetch_mldata('MNIST original')
 a = mnist.data.shape
 b = mnist.target.shape
 c = np.unique(mnist.target)
-
+d = mnist.DESCR
 print(a)
 print(b)
 print(c)
+print(d)
 # Question 1
 # Set up:
 # spliting the test and training set for k fold cross coreletion with a 10 fold
@@ -40,6 +41,30 @@ def activation(weights, inputs):
     for i in range(len(weights)-1):
         activation +=weights[i]*inputs[i]
     return activation
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def deSigmoid(y):
+    return y*(1-y)
+
+def output(node):
+    return sigmoid(activation(node.weights,node.inputs))
+
+def correct(out,error):
+    return error*out*(1-out)
+
+def error(out,expected):
+    return expected-out
+
+def backCorrection(weights,lastCorrect,out):
+    correctionSum=0
+    for i in range(len(weights)-1):
+        correctionSum +=weights[i]*lastCorrect
+    return out*(1-out) * correctionSum
+
+def newWeight(weight,out,correct):
+    return weight+.5*out*correct
 
 
 
@@ -67,6 +92,17 @@ class NeuralNetwork(object):
 
 
 print("Question 1: ")
+start_time = time.time()
+results = run()
+end_time = time.time()
+print "Overall running time:", end_time - start_time
+for train_index, test_index in kf.split(X):
+    print("TRAIN:", train_index, "TEST:", test_index)
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    #print(len(train_index))
+    print("TRAIN size:", len(train_index), "TEST size:", len(test_index))
+    # in here do the work with testing with the k fold train and test
 
 
 
