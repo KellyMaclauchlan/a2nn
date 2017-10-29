6cb7bd1b26234e44492e06155209d248a5556d8c
Kelly Maclauchlan <kelly maclauchlan>
2017-10-29 13:54:04 -0400

    added kmeans and nearest neghbor along with function for hidden layer

--- a2q2.py
+++ a2q2.py
@@ -3,10 +3,26 @@ from scipy import stats
 from sklearn.datasets import fetch_mldata
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
+from sklearn.cluster import KMeans
+from sklearn.neighbors import NearestNeighbors
 from sklearn import svm
 import random
 import time
+import math
 
+mnist = fetch_mldata('MNIST original')
+kf = KFold(n_splits=10, shuffle=True);
+X=mnist.data
+y=mnist.target
+
+def getR(dists,k):
+	sumSquared=0
+	for i in dist:
+		sumSquared+=i*i
+	return math.sqrt(sumSquared/k)
+
+def hiddenFunc(X,i):
+	return math.exp(-(((X-centers[i])**2)/r[i]**2))
 
 
 class outputNode(object):
@@ -23,3 +39,26 @@ class rbfNetwork(object):
 
 
 net = rbfNetwork(10,784,256)
+
+kmeans = KMeans(n_clusters=15, random_state=0).fit(X)
+
+centers = kmeans.cluster_centers_
+
+print(centers)
+print(len(centers))
+
+neigh=NearestNeighbors(n_neighbors=20)
+neigh.fit(X)
+
+dists,indexs = neigh.kneighbors(X=centers)
+
+r=[]
+for dist in dists:
+	r.append(getR(dist,20))
+
+print(r)
+
+
+
+
+
